49564cbb06611601ee38049f7d607f8770e49d76
Joseph Duffy <commits@josephduffy.co.uk>
2020-04-13 20:42:55 +0100

    Format jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: "ts-jest",
   roots: ["src/"],
   testPathIgnorePatterns: ["/src/__tests__/helpers/"],
-  collectCoverageFrom: ["src/{!(__tests___),}.ts"]
+  collectCoverageFrom: ["src/{!(__tests___),}.ts"],
 }
