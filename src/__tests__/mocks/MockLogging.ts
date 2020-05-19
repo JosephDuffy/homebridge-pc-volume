cd5e0983aeb6d6ac59dc602eb736753e69713840
Joseph Duffy <commits@josephduffy.co.uk>
2020-05-19 23:33:26 +0100

    Fix linting issues

--- src/__tests__/mocks/MockLogging.ts
+++ src/__tests__/mocks/MockLogging.ts
@@ -3,13 +3,16 @@ import { LogLevel } from "homebridge"
 export default class MockLogging {
   prefix: string
 
-  constructor(prefix: string = "") {
+  constructor(prefix = "") {
     this.prefix = prefix
   }
 
-  info(message: string, ...parameters: any[]) {}
-  warn(message: string, ...parameters: any[]) {}
-  error(message: string, ...parameters: any[]) {}
-  debug(message: string, ...parameters: any[]) {}
-  log(level: LogLevel, message: string, ...parameters: any[]) {}
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  /* eslint-disable @typescript-eslint/no-explicit-any */
+  /* eslint-disable @typescript-eslint/no-empty-function */
+  info(message: string, ...parameters: any[]): void {}
+  warn(message: string, ...parameters: any[]): void {}
+  error(message: string, ...parameters: any[]): void {}
+  debug(message: string, ...parameters: any[]): void {}
+  log(level: LogLevel, message: string, ...parameters: any[]): void {}
 }
