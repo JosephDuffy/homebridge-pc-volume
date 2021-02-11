8206da7eecdd0a51bb0eb5f2cb73fb4fb3c5f785
Joseph Duffy <commits@josephduffy.co.uk>
2021-02-11 14:15:53 +0000

    Add types for configs

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   preset: "ts-jest",
   roots: ["src/"],
