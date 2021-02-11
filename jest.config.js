3fe6a94d012026ba1f393fffcc28630f79594b13
Joseph Duffy <commits@josephduffy.co.uk>
2021-02-11 14:25:56 +0000

    Set jest test environment

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
   preset: "ts-jest",
+  testEnvironment: "node",
   roots: ["src/"],
   testPathIgnorePatterns: ["/src/__tests__/mocks/"],
   collectCoverageFrom: ["src/{!(__tests___),}.ts"],
