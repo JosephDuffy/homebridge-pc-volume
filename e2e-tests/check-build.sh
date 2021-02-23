e57ed6330006160567086a50f0f141a427ed35dc
Joseph Duffy <commits@josephduffy.co.uk>
2021-02-23 23:25:47 +0000

    Sync e2e tests/dev deps Homebridge versions

--- e2e-tests/check-build.sh
+++ e2e-tests/check-build.sh
@@ -5,6 +5,5 @@ set timeout 600
 spawn docker run homebridge-pc-volume-e2e
 expect {
     "ERROR LOADING PLUGIN" {exit 1}
-    # Version 1.2.3 used to output "Homebridge v1.2.3 is running", but now appears to output "Homebridge is running" so both are accepted. This would also match version like `v1.2.` but 🤷‍♂️
-    -re {Homebridge( v[0-9]+[0-9.]*)? is running}
+    -re {Homebridge is running}
 }
--- e2e-tests/test.sh
+++ e2e-tests/test.sh
@@ -3,7 +3,8 @@
 npm run-script build
 npm pack
 tar zxvf homebridge-pc-volume-*.tgz -C "e2e-tests"
-cd e2e-tests
-cp ../.homebridge-debug/config.json ./config.json
+cd e2e-tests || exit
+cp ../.homebridge-debug/config.json ./
+cp ../package-lock.json ./
 docker build . -t homebridge-pc-volume-e2e
 ./check-build.sh
